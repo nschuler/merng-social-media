4bccb3135c91fdf8bdad14684519dd773d870a98
nschuler <nicholas.schuler@student.uts.edu.au>
2020-12-27 00:59:32 +1100

    Create & Delete Posts resolvers

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -7,7 +7,7 @@ const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
 function generateToken(user){
-    jwt.sign({
+    return jwt.sign({
         id: user.id,
         email: user.email,
         username: user.username
